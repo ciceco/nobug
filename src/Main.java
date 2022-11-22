7a374ea3d5e02d6432e6daafa47674331ed09dcd
cswag <1423463108@qq.com>
2022-11-22 10:48:49 +0800

    123

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("nobug-add content  2");
+        System.out.println("nobug-add   content  2");
     }
 }
